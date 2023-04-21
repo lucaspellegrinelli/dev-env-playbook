075ab09d91e974a3a5b824e4fcb14892a412e55f
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-04-21 10:19:12 -0300

    Fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install -y sudo software-properties-common git build-essential;
+sudo apt-get install -y sudo software-properties-common git curl build-essential;
 sudo apt-add-repository -y ppa:ansible/ansible;
 sudo apt-get update;
 sudo apt-get install -y ansible;
